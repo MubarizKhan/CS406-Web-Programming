e9b94edd5d4ba77f758d015e66ced793f22a7227
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-13 23:54:37 +0500

    Delete variables.js

--- Javascript/variables.js
+++
@@ -1,8 +0,0 @@
-// Variables are simply containers that store values; The follow this pattern
-
-var yourvariablename = "your value" //can be anything
-
-//All sorts of data types can be stored in this
-var name = "Mubariz";
-var fav_num= "13";
-var ishuman = true;
