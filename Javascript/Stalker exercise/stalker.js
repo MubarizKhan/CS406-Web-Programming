1699672c4cbff444b9dfb03ddbcf2d00ba75972c
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-14 00:29:10 +0500

    Add files via upload

---
+++ Javascript/Stalker exercise/stalker.js
@@ -0,0 +1,10 @@
+prompt("what is your first name")
+var firstname = prompt("what is your first name");
+
+prompt("what is your last name?")
+var lastname = prompt("what is your last name?");
+
+prompt("what is your age?")
+var age = prompt("what is your age?");
+
+alert("hello my dear" + firstname + lastname + " I see that you're" + age)
