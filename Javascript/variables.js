94670d3bd95d8fda8242a12cda7c623bfd075fb5
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-13 23:53:54 +0500

    Add files via upload

---
+++ Javascript/variables.js
@@ -0,0 +1,8 @@
+// Variables are simply containers that store values; The follow this pattern
+
+var yourvariablename = "your value" //can be anything
+
+//All sorts of data types can be stored in this
+var name = "Mubariz";
+var fav_num= "13";
+var ishuman = true;
