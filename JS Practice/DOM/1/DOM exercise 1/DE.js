6a00f68e410cb258db30b9ae77e3767093443559
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-09-22 20:17:12 +0500

    Add files via upload

---
+++ JS Practice/DOM/1/DOM exercise 1/DE.js
@@ -0,0 +1,11 @@
+// Come up with 4 diff ways to select the first <P> tag
+var var1 = document.getElementById("first");
+var var2 = document.getElementsByTagName("p");
+var var3 = document.getElementsByClassName("special");
+var var4 = document.querySelector("P")
+
+
+console.log(var3);
+
+
+alert("hi>");
