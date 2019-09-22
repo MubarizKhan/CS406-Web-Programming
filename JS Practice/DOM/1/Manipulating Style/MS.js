aa35fbf1d8b4ae445882f2bcdacfbe8bbf7f9e70
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-09-22 21:58:16 +0500

    Update MS.js

--- JS Practice/DOM/1/Manipulating Style/MS.js
+++ JS Practice/DOM/1/Manipulating Style/MS.js
@@ -1,4 +1,28 @@
-var p = document.querySelector("p")
-p.classList.add("big")
-p.classList.remove("big")
-p.classList.toggle("big")
+// var p = document.querySelector("p")
+// p.classList.add("big")
+// p.classList.remove("big")
+// p.classList.toggle("big")
+
+var p = document.getElementsByTagName("p")[0]
+var ul = document.querySelector("ul")
+
+// console.log(ul);
+// ul.innerHTML
+
+p.textContent = "Van Burren";
+
+var x = document.getElementsByTagName("img")[0]
+console.log(x);
+console.log(x.getAttribute("src"));
+
+x.setAttribute("src", "3.jpg");
+
+
+var x1 = document.getElementsByTagName("img")[1]
+x1.setAttribute("src", "1.jpg");
+
+var xy = document.querySelector("a");
+xy.setAttribute("href", "http://www.facebook.com");
+console.log(xy);
+
+xy.textContent = "link to facebook.com"
