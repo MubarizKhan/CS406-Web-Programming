13eddfd63499b607cb90dbe41b2502cf7c26a256
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-09-22 20:16:37 +0500

    Add files via upload

---
+++ JS Practice/DOM/1/2/2.js
@@ -0,0 +1,24 @@
+var tag = document.getElementById("highlight")
+console.log(tag);
+// console.dir(tag);
+
+var cle = document.getElementsByClassName("bolded")
+// cle.style.color = "pink";
+console.log(cle[1]);
+
+var tags = document.getElementsByTagName("h1")
+console.log(tags)
+
+var h1 = document.querySelector("h1")
+h1.style.color = "blue";
+
+
+var li = document.querySelector("#highlight")
+li.style.color = "blue";
+console.log(li);
+
+var lis = document.querySelectorAll("h3")
+// lis.style.color = "yellow";
+console.log(lis)
+
+// var ti = document.querySelector("li a.bolded")?
