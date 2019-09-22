5687e2735517ae7f0259aff92a27aaba0956434a
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-09-22 12:40:29 +0500

    Add files via upload

---
+++ JS Practice/conditionals.js
@@ -0,0 +1,36 @@
+prompt("What is your age? ");
+var age = prompt("what is your age?");
+
+if (age < 0)
+{
+    alert("You're not born? smh");
+}
+
+else if(age == 21)
+{
+    alert("Happy Birthday");
+    
+    if((age * age) / age == age)
+    {
+        alert("your age is a perfect square");
+    } 
+}
+
+prompt("guess a number")
+var number = prompt("guess the number?");
+
+guess = 7;
+if(number < 7)
+{
+    alert("Too low bro");
+}
+
+else if(number > 7)
+{
+    alert("Too high");
+}
+
+else
+{
+    alert("you guessed it!");
+}
---
+++ JS Practice/functions.js
@@ -0,0 +1,39 @@
+function dosomething()
+{
+    console.log("Do something");
+}
+
+dosomething();
+
+function hi()
+{
+  console.log("Do something with this life");
+  
+}
+
+
+function square(num)
+{
+  alert(num * num);
+  
+}
+
+function greet(user1, user2, user3)
+{
+  console.log("greetings" + user1);
+  console.log("hi" + user2);
+  console.log("hello" + user3);
+  
+}
+
+greet("saad", "mubariz", "mubeen")
+
+var name = "mubariz";
+var favnum = "13";
+var ishuman = "true";
+
+var name = null;
+
+
+alert("hello fellow code viewer!");
+alert(99 * 345);
